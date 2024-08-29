27e31c91d3cf98e443933a473d2067a89f8e0358
JaimeNarvaez <jaimeangel936@gmail.com>
2024-08-28 20:16:32 -0500

    Cambios del 28/08 en bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,11 @@ intents = discord.Intents.default()
 intents.message_content = True
 # Crear un bot en la variable cliente y transferirle los privilegios
 bot = commands.Bot(command_prefix="$", intents=intents)
-
+#ESto es un cambio
 @bot.event
 async def on_ready():
     print(f'Hemos iniciado sesión como {bot.user}')
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("hi")
 
 @bot.command()    
 async def bye(ctx):
